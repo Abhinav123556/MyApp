e1ff66f745cb2df13549d3b10d0a1dbef50abb59
Abhinav Rao <abhinavrao2690@gmail.com>
2025-12-17 10:32:27 +0530

    CI/CD Pipeline

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,37 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  myForm: FormGroup;
-  weatherData: any;
-
-  constructor(private fb: FormBuilder, private http: HttpClient) {
-    this.myForm = this.fb.group({
-      city: ['']
-    });
-  }
-
-  ngOnInit() {
-    // Example API call
-    this.http.get<any>('/weather').subscribe((data: any) => {
-      this.weatherData = data;
-      console.log('Weather data:', data);
-    });
-  }
-
-  submitForm() {
-    const city = this.myForm.value.city;
-    console.log('Form submitted. City:', city);
-
-    // Example: fetch weather for entered city
-    this.http.get<any>(`/weather?city=${city}`).subscribe((data: any) => {
-      this.weatherData = data;
-      console.log('Weather data for city:', data);
-    });
-  }
-}
+export class AppComponent {}
